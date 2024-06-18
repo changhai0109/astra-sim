b71fd2ebb81477590b473db7e47ed680b210396f
Changhai Man <changhai0109@hotmail.com>
2024-06-18 09:49:17 -0700

    local memory tracker add report function

--- astra-sim/workload/LocalMemoryTracker.cc
+++ astra-sim/workload/LocalMemoryTracker.cc
@@ -1,5 +1,6 @@
 #include "astra-sim/workload/LocalMemoryTracker.hh"
 
+#include <filesystem>
 #include <map>
 #include <memory>
 #include <string>
@@ -9,8 +10,10 @@
 #include "astra-sim/system/Sys.hh"
 #include "et_feeder/et_feeder.h"
 #include "et_feeder/et_feeder_node.h"
+#include "json/json.hpp"
 
 using namespace AstraSim;
+using json = nlohmann::json;
 
 LocalMemoryTracker::LocalMemoryTracker(
     Workload* workload,
@@ -24,36 +27,48 @@ LocalMemoryTracker::LocalMemoryTracker(
 void LocalMemoryTracker::issueNode(
     Chakra::ETFeeder* etFeeder,
     std::shared_ptr<Chakra::ETFeederNode> node) {
+  auto logger = LoggerFactory::get_logger("local-memory-tracker");
   Tick now = Sys::boostedTick();
   size_t tensorSize = node->tensor_size();
   this->memoryContentSize += tensorSize;
   memoryUsageTimeline.insert({now, this->memoryContentSize});
   if (this->trackMemoryActivities) {
-    MemoryActivity writeActivity = {MemoryActivityType::WRITE_START, node, now};
+    MemoryActivity writeActivity = {
+        MemoryActivityType::WRITE_START, node, node, now};
     this->memoryActivities.push_back(writeActivity);
-    MemoryActivity allocActivity = {MemoryActivityType::ALLOC, node, now};
+    MemoryActivity allocActivity = {MemoryActivityType::ALLOC, node, node, now};
     this->memoryActivities.push_back(allocActivity);
     for (auto parentId : node->getChakraNode()->data_deps()) {
       auto parent = etFeeder->lookupNode(parentId);
       MemoryActivity readActivity = {
-          MemoryActivityType::READ_START, parent, now};
+          MemoryActivityType::READ_START, parent, node, now};
       this->memoryActivities.push_back(readActivity);
     }
   }
   if (this->memoryContentSize > this->peakMemoryUsage)
     this->peakMemoryUsage = this->memoryContentSize;
+  logger->trace(
+      "issue, tick={} sys.id={} node.id={} node.name={} memoryContentSize={}",
+      Sys::boostedTick(),
+      this->workload->sys->id,
+      node->id(),
+      node->name(),
+      this->memoryContentSize);
 }
 
 void LocalMemoryTracker::finishedNode(
     Chakra::ETFeeder* etFeeder,
     std::shared_ptr<Chakra::ETFeederNode> node) {
+  auto logger = LoggerFactory::get_logger("local-memory-tracker");
   Tick now = Sys::boostedTick();
   if (this->trackMemoryActivities) {
-    MemoryActivity writeActivity = {MemoryActivityType::WRITE_END, node, now};
+    MemoryActivity writeActivity = {
+        MemoryActivityType::WRITE_END, node, node, now};
     this->memoryActivities.push_back(writeActivity);
     for (auto parentId : node->getChakraNode()->data_deps()) {
       auto parent = etFeeder->lookupNode(parentId);
-      MemoryActivity readActivity = {MemoryActivityType::READ_END, parent, now};
+      MemoryActivity readActivity = {
+          MemoryActivityType::READ_END, parent, node, now};
       this->memoryActivities.push_back(readActivity);
     }
   }
@@ -71,17 +86,96 @@ void LocalMemoryTracker::finishedNode(
       this->memoryContentSize -= parent->tensor_size();
       this->memoryUsageTimeline.insert({now, this->memoryContentSize});
       if (this->trackMemoryActivities) {
-        MemoryActivity freeActivity = {MemoryActivityType::FREE, node, now};
+        MemoryActivity freeActivity = {
+            MemoryActivityType::FREE, parent, node, now};
         this->memoryActivities.push_back(freeActivity);
+        etFeeder->removeNode(parent->id());
       }
     }
   }
+  logger->trace(
+      "callback, tick={} sys.id={} node.id={} node.name={} memoryContentSize={}",
+      Sys::boostedTick(),
+      this->workload->sys->id,
+      node->id(),
+      node->name(),
+      this->memoryContentSize);
 }
 
-void LocalMemoryTracker::report(std::string reportPath) {
+void LocalMemoryTracker::report(std::string reportFolder) {
   auto logger = LoggerFactory::get_logger("local-memory-tracker");
   logger->info(
       "sys->id={}, peak memory usage={}",
       this->workload->sys->id,
       this->peakMemoryUsage);
+
+  // report memory activities
+  if (this->workload->sys->track_mem_activities) {
+    json j;
+    j["traceEvents"] = std::vector<json>();
+    for (auto activity : this->memoryActivities) {
+      auto node = activity.dataOwner;
+      std::string ph, cat, type;
+      switch (activity.type) {
+        case READ_START:
+          ph = "B";
+          cat = "memory_activities";
+          type = "read";
+          break;
+        case WRITE_START:
+          ph = "B";
+          cat = "memory_activities";
+          type = "write";
+          break;
+        case ALLOC:
+          ph = "B";
+          cat = "memory_lifetime";
+          type = "alloc";
+          break;
+        case READ_END:
+          ph = "E";
+          cat = "memory_activities";
+          type = "read";
+          break;
+        case WRITE_END:
+          ph = "E";
+          cat = "memory_activities";
+          type = "write";
+          break;
+        case FREE:
+          ph = "E";
+          cat = "memory_lifetime";
+          type = "alloc";
+          break;
+        default:
+          Sys::sys_panic("");
+      }
+      json event = {
+          {"name", node->name()},
+          {"cat", "memory activities"},
+          {"ph", ph},
+          {"ts", 1e-3 * activity.tick},
+          {"pid", this->workload->sys->id},
+          {"tid", node->id()},
+          {"args",
+           json{
+               {"size", node->tensor_size()},
+               {"type", type},
+               {"caller", activity.caller->name()}}}};
+      j["traceEvents"].push_back(event);
+    }
+
+    auto chromeTracePath = reportFolder + "/" + "chrome_trace." +
+        std::to_string(this->workload->sys->id) + ".json";
+    std::ofstream file(chromeTracePath);
+    file << j.dump();
+    // logger->info(j.dump());
+    file.close();
+  }
+}
+
+LocalMemoryTracker::~LocalMemoryTracker() {
+  this->memoryActivities.clear();
+  this->memoryUsageTimeline.clear();
+  this->releasedNodes.clear();
 }
